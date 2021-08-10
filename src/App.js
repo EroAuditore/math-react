b110ccc8d725d735c5656b70271e5a888cfdce32
eroauditore <alfa.fenrir@gmail.com>
2021-08-10 16:37:49 -0500

    App component into class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Container>
-      <Calculator />
-    </Container>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container>
+        <Calculator />
+      </Container>
+    );
+  }
 }
 
 export default App;
