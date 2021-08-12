5f8ca1550cbe38d8f7c88646500287c8268e97a2
eroauditore <alfa.fenrir@gmail.com>
2021-08-11 23:33:59 -0500

    Calcule functoin added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import Calculator from './components/Calculator';
+import calculate from './Logic/calculate';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: '',
+      result: '0',
+      operation: null,
+    };
   }
 
+  resultDisplay = (result) => {
+    if (result.next) {
+      return result.next;
+    }
+    if (result.total !== null) {
+      return result.total;
+    }
+    return '0';
+  };
+
+  handleClick = (btnName) => {
+    const { next, total, operation } = this.state;
+
+    const obj = {
+      total,
+      next,
+      operation,
+    };
+    const result = calculate(obj, btnName);
+    this.setState({
+      result: this.resultDisplay(result),
+      next: result.next,
+      total: result.total,
+      operation: result.operation,
+    });
+    console.log(result);
+  };
+
   render() {
+    const { total, next, result } = this.state;
     return (
       <Container>
-        <Calculator />
+        <Calculator
+          handleClick={(btnName) => this.handleClick(btnName)}
+          total={total}
+          next={next}
+          result={result}
+        />
       </Container>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Col, Row, Button } from 'react-bootstrap';
 
 class Calculator extends Component {
@@ -8,116 +9,196 @@ class Calculator extends Component {
   }
 
   render() {
+    const { handleClick, result } = this.props;
+
     return (
       <>
         <h2>Calculator:</h2>
         <Row>
           <Col xs={12} className="p-0">
-            <div className="bg-secondary text-end text-white pe-3"> 0</div>
+            <div className="bg-secondary text-end text-white pe-3">
+              {result}
+            </div>
           </Col>
         </Row>
         <Row>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('AC')}
+            >
               AC
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('+/-')}
+            >
               +/-
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('%')}
+            >
               %
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="warning" className="w-100">
-              %
+            <Button
+              variant="warning"
+              className="w-100"
+              onClick={() => handleClick('/')}
+            >
+              /
             </Button>
           </Col>
         </Row>
         <Row>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('7')}
+            >
               7
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('8')}
+            >
               8
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('9')}
+            >
               9
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="warning" className="w-100">
+            <Button
+              variant="warning"
+              className="w-100"
+              onClick={() => handleClick('x')}
+            >
               x
             </Button>
           </Col>
         </Row>
         <Row>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('4')}
+            >
               4
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('5')}
+            >
               5
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('6')}
+            >
               6
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="warning" className="w-100">
-              +
+            <Button
+              variant="warning"
+              className="w-100"
+              onClick={() => handleClick('-')}
+            >
+              -
             </Button>
           </Col>
         </Row>
         <Row>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('1')}
+            >
               1
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('2')}
+            >
               2
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('3')}
+            >
               3
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="warning" className="w-100">
+            <Button
+              variant="warning"
+              className="w-100"
+              onClick={() => handleClick('+')}
+            >
               +
             </Button>
           </Col>
         </Row>
         <Row>
           <Col xs={6} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('0')}
+            >
               0
             </Button>
           </Col>
           <Col xs={3} className="p-0">
-            <Button variant="secondary" className="w-100">
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => handleClick('.')}
+            >
               .
             </Button>
           </Col>
 
           <Col xs={3} className="p-0">
-            <Button variant="warning" className="w-100">
+            <Button
+              variant="warning"
+              className="w-100"
+              onClick={() => handleClick('=')}
+            >
               =
             </Button>
           </Col>
@@ -126,5 +207,11 @@ class Calculator extends Component {
     );
   }
 }
+
+Calculator.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  total: PropTypes.string,
+  next: PropTypes.string,
+};
 
 export default Calculator;
