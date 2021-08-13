a1fead45deb5bb31adb9e85647f4f0f380a4ce2a
eroauditore <alfa.fenrir@gmail.com>
2021-08-12 22:26:46 -0500

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Container className="d-flex justify-content-center pt-4">
-        <div className="calc-container">
-          <Calculator />
-        </div>
-      </Container>
-    );
-  }
-}
+const App = () => (
+  <Container className="d-flex justify-content-center pt-4">
+    <div className="calc-container">
+      <Calculator />
+    </div>
+  </Container>
+);
 
 export default App;
